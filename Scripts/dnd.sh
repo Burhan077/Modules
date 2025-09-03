1b549a7ff462fc6e483d9168bd5a351eff436701
Burhanudeen <ahmed.b.rashid07@gmail.com>
2025-09-03 18:02:03 +0300

    Update dnd.sh

--- Scripts/dnd.sh
+++ Scripts/dnd.sh
@@ -2,7 +2,7 @@
 
 # ------------ CONFIG ----------------
 notify_id=929292
-icon_dir="~/.config/dunst/icons/notifications"
+icon_dir="$HOME/.config/dunst/icons/notifications"
 notify_timeout=5000  # 5 seconds
 state_file="/tmp/.dndstate"  # Fallback for non-dunstctl use
 # ------------------------------------
